5638b7da394dc8aa128731bc6dfdfc90a530abee
Marcus Bernt <berntmarcus86@gmail.com>
2022-11-21 15:45:43 +0100

    [+] refactored processing of default shipping method

--- Plugin/Checkout/Model/DefaultConfigProviderPlugin.php
+++ Plugin/Checkout/Model/DefaultConfigProviderPlugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   Mediarox_InstantEstimatedShipping
  * @copyright Copyright 2022 (c) mediarox UG (haftungsbeschraenkt) (http://www.mediarox.de)
@@ -11,207 +12,198 @@ namespace Mediarox\InstantEstimatedShipping\Plugin\Checkout\Model;
 
 use Magento\Checkout\Model\DefaultConfigProvider;
 use Magento\Checkout\Model\Session;
+use Magento\Customer\Api\Data\CustomerInterface;
 use Magento\Directory\Helper\Data;
-use Magento\Framework\Exception\CouldNotSaveException;
-use Magento\Framework\Exception\InputException;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Exception\StateException;
 use Magento\Quote\Api\Data\AddressInterface;
+use Magento\Quote\Api\Data\CartExtensionFactory;
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Quote\Api\ShippingMethodManagementInterface;
 use Magento\Quote\Model\Quote;
-use Magento\Quote\Model\ShippingMethodManagement;
+use Magento\Quote\Model\ShippingAssignmentFactory;
+use Magento\Quote\Model\ShippingFactory;
+use Magento\Store\Model\StoreManagerInterface;
 
 class DefaultConfigProviderPlugin
 {
     protected Session $checkoutSession;
-    protected Quote\Address\FreeShippingInterface $freeShipping;
-    protected ShippingMethodManagement $shippingMethodManagement;
     protected Data $directoryHelper;
+    protected StoreManagerInterface $storeManager;
+    protected CartExtensionFactory $cartExtensionFactory;
+    protected ShippingAssignmentFactory $assignmentFactory;
+    protected ShippingFactory $shippingFactory;
+    protected ShippingMethodManagementInterface $methodManagement;
+    protected array $addressMethods;
 
     public function __construct(
         Session $checkoutSession,
-        Quote\Address\FreeShippingInterface $freeShipping,
-        ShippingMethodManagement $shippingMethodManagement,
-        Data $directoryHelper
+        Data $directoryHelper,
+        StoreManagerInterface $storeManager,
+        CartExtensionFactory $cartExtensionFactory,
+        ShippingAssignmentFactory $assignmentFactory,
+        ShippingFactory $shippingFactory,
+        ShippingMethodManagementInterface $methodManagement
     ) {
         $this->checkoutSession = $checkoutSession;
-        $this->freeShipping = $freeShipping;
-        $this->shippingMethodManagement = $shippingMethodManagement;
         $this->directoryHelper = $directoryHelper;
-    }
-
-    /**
-     * Gets shipping assignments data like items weight, address weight, items quantity.
-     *
-     * @param  AddressInterface $address
-     * @param  array            $items
-     * @return array
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     */
-    private function getAssignmentWeightData(AddressInterface $address, array $items): array
-    {
-        $address->setWeight(0);
-        $address->setFreeMethodWeight(0);
-        $addressWeight = $address->getWeight();
-        $freeMethodWeight = $address->getFreeMethodWeight();
-        $addressFreeShipping = (bool)$address->getFreeShipping();
-        $addressQty = 0;
-        foreach ($items as $item) {
-            /**
-             * Skip if this item is virtual
-             */
-            if ($item->getProduct()->isVirtual()) {
-                continue;
-            }
-
-            /**
-             * Children weight we calculate for parent
-             */
-            if ($item->getParentItem()) {
-                continue;
-            }
-
-            $itemQty = (float)$item->getQty();
-            $itemWeight = (float)$item->getWeight();
-
-            if ($item->getHasChildren() && $item->isShipSeparately()) {
-                foreach ($item->getChildren() as $child) {
-                    if ($child->getProduct()->isVirtual()) {
-                        continue;
-                    }
-                    $addressQty += $child->getTotalQty();
-
-                    if (!$item->getProduct()->getWeightType()) {
-                        $itemWeight = (float)$child->getWeight();
-                        $itemQty = (float)$child->getTotalQty();
-                        $addressWeight += ($itemWeight * $itemQty);
-                        $rowWeight = $this->getItemRowWeight(
-                            $addressFreeShipping,
-                            $itemWeight,
-                            $itemQty,
-                            $child->getFreeShipping()
-                        );
-                        $freeMethodWeight += $rowWeight;
-                        $item->setRowWeight($rowWeight);
-                    }
-                }
-                if ($item->getProduct()->getWeightType()) {
-                    $addressWeight += ($itemWeight * $itemQty);
-                    $rowWeight = $this->getItemRowWeight(
-                        $addressFreeShipping,
-                        $itemWeight,
-                        $itemQty,
-                        $item->getFreeShipping()
-                    );
-                    $freeMethodWeight += $rowWeight;
-                    $item->setRowWeight($rowWeight);
-                }
-            } else {
-                if (!$item->getProduct()->isVirtual()) {
-                    $addressQty += $itemQty;
-                }
-                $addressWeight += ($itemWeight * $itemQty);
-                $rowWeight = $this->getItemRowWeight(
-                    $addressFreeShipping,
-                    $itemWeight,
-                    $itemQty,
-                    $item->getFreeShipping()
-                );
-                $freeMethodWeight += $rowWeight;
-                $item->setRowWeight($rowWeight);
-            }
-        }
-
-        return [
-            'addressQty'       => $addressQty,
-            'addressWeight'    => $addressWeight,
-            'freeMethodWeight' => $freeMethodWeight,
-        ];
-    }
-
-    /**
-     * Calculates item row weight.
-     *
-     * @param  bool  $addressFreeShipping
-     * @param  float $itemWeight
-     * @param  float $itemQty
-     * @param  bool  $freeShipping
-     * @return float
-     */
-    private function getItemRowWeight(
-        bool $addressFreeShipping,
-        float $itemWeight,
-        float $itemQty,
-        $freeShipping
-    ): float {
-        $rowWeight = $itemWeight * $itemQty;
-        if ($addressFreeShipping || $freeShipping === true) {
-            $rowWeight = 0;
-        } elseif (is_numeric($freeShipping)) {
-            $freeQty = $freeShipping;
-            if ($itemQty > $freeQty) {
-                $rowWeight = $itemWeight * ($itemQty - $freeQty);
-            } else {
-                $rowWeight = 0;
-            }
-        }
-        return (float)$rowWeight;
-    }
-
-    private function setShippingAddressValues(Quote\Address $shippingAddress, Quote $quote)
-    {
-        $items = $shippingAddress->getAllItems();
-        $data = $this->getAssignmentWeightData($shippingAddress, $items);
-
-        $shippingAddress->setItemQty($data['addressQty']);
-        $shippingAddress->setWeight($data['addressWeight']);
-        $shippingAddress->setFreeMethodWeight($data['freeMethodWeight']);
-        $shippingAddressFreeShipping = (bool)$shippingAddress->getFreeShipping();
-        $isFreeShipping = $this->freeShipping->isFreeShipping($quote, $quote->getAllItems());
-        $shippingAddress->setFreeShipping($isFreeShipping);
-        if (!$shippingAddressFreeShipping && $isFreeShipping) {
-            $data = $this->getAssignmentWeightData($shippingAddress, $items);
-            $shippingAddress->setItemQty($data['addressQty']);
-            $shippingAddress->setWeight($data['addressWeight']);
-            $shippingAddress->setFreeMethodWeight($data['freeMethodWeight']);
-        }
-        $shippingAddress->setCollectShippingRates(true);
-        $shippingAddress->collectShippingRates();
+        $this->storeManager = $storeManager;
+        $this->cartExtensionFactory = $cartExtensionFactory;
+        $this->assignmentFactory = $assignmentFactory;
+        $this->shippingFactory = $shippingFactory;
+        $this->methodManagement = $methodManagement;
     }
 
     public function beforeGetConfig(DefaultConfigProvider $subject)
     {
         $quote = $this->checkoutSession->getQuote();
+        $shippingAddress = $this->getShippingAddress($quote);
+        $this->processShippingAssignment($quote, $shippingAddress);
+    }
+
+    private function getShippingAddress(Quote $quote)
+    {
+        if ($quote->getCustomerId() && $shippingAddress = $this->getCustomerShippingAddress($quote)) {
+            return $shippingAddress;
+        }
+
         $shippingAddress = $quote->getShippingAddress();
-        if ($shippingAddress->getShippingMethod()) {
-            return null;
+        if ($shippingAddress->getId() && $shippingAddress->getCountryId()) {
+            return $shippingAddress;
         }
 
         if (!$shippingAddress->getCountryId()) {
-            $shippingAddress->setCountryId($this->directoryHelper->getDefaultCountry());
+            $shippingAddress->setCountryId($this->directoryHelper->getDefaultCountry($this->storeManager->getStore()));
+        }
+        return $shippingAddress;
+    }
+
+    private function getCustomerShippingAddress(Quote $quote)
+    {
+        $shippingAddress = $quote->getShippingAddress();
+        if ($shippingAddress->getId() && $shippingAddress->getCustomerAddressId()) {
+            return $shippingAddress;
         }
 
-        $this->setShippingAddressValues($shippingAddress, $quote);
-        $bestRate = null;
-        $rates = $shippingAddress->getAllShippingRates();
-        if ($rates) {
-            // sort highest to lowest rate
-            usort($rates, function ($rate1, $rate2) {
-                return $rate2->getPrice() <=> $rate1->getPrice();
-            });
-            $bestRate = array_shift($rates);
+        $customerAddress = $this->getCustomerAddress($quote->getCustomer());
+
+        if (!$customerAddress) {
+            return false;
         }
-        if ($bestRate) {
-            try {
-                $this->shippingMethodManagement->set($quote->getId(), $bestRate->getCarrier(), $bestRate->getMethod());
-            } catch (CouldNotSaveException | InputException | NoSuchEntityException | StateException $e) {
-                $quote->addErrorInfo(
-                    'error',
-                    self::class . '::' . __METHOD__,
-                    $e->getCode(),
-                    $e->getMessage()
-                );
+
+        $addressByCustomer = $quote->getShippingAddressByCustomerAddressId($customerAddress->getId());
+        if ($addressByCustomer) {
+            $quote->setShippingAddress($addressByCustomer);
+            return $addressByCustomer;
+        }
+
+        $shippingAddress->importCustomerAddressData($customerAddress);
+
+        return $shippingAddress;
+    }
+
+    private function getCustomerAddress(CustomerInterface $customer)
+    {
+        $customer->getDefaultShipping();
+        $addresses = $customer->getAddresses();
+        if (empty($addresses)) {
+            return false;
+        }
+
+        foreach ($addresses as $address) {
+            if ($address->isDefaultShipping()) {
+                return $address;
             }
         }
-        return null;
+
+        return reset($addresses);
+    }
+
+    public function processShippingAssignment(CartInterface $quote, AddressInterface $shippingAddress)
+    {
+        $shippingMethod = $this->getDefaultShippingMethod($shippingAddress);
+
+        $cartExtension = $quote->getExtensionAttributes();
+        if ($cartExtension === null) {
+            $cartExtension = $this->cartExtensionFactory->create();
+        }
+
+        $shippingAssignments = $cartExtension->getShippingAssignments();
+        if (empty($shippingAssignments)) {
+            $shippingAssignment = $this->assignmentFactory->create();
+        } else {
+            $shippingAssignment = $shippingAssignments[0];
+        }
+
+        if (!$shippingMethod) {
+            $cartExtension->setShippingAssignments([]);
+            $shippingAddress->setShippingMethod(null);
+
+            return $quote->setExtensionAttributes($cartExtension);
+        }
+
+        $shipping = $shippingAssignment->getShipping();
+        if ($shipping === null) {
+            $shipping = $this->shippingFactory->create();
+        }
+
+        $carrierCode = $shippingMethod->getCarrierCode();
+        $shippingAddress->setLimitCarrier($carrierCode);
+        $methodCode = $shippingMethod->getMethodCode();
+        $method = $carrierCode . '_' . $methodCode;
+        $shippingAddress->setShippingMethod($method);
+        $shipping->setAddress($shippingAddress);
+        $shipping->setMethod($method);
+        $shippingAssignment->setShipping($shipping);
+        $cartExtension->setShippingAssignments([$shippingAssignment]);
+        $quote->setTotalsCollectedFlag(false);
+
+        return $quote->setExtensionAttributes($cartExtension);
+    }
+
+    private function getDefaultShippingMethod(AddressInterface $shippingAddress)
+    {
+        $methods = $this->getShippingMethods($shippingAddress);
+        foreach ($methods as $key => $method) {
+            if (!$method->getAvailable()) {
+                unset($methods[$key]);
+            }
+        }
+
+        if (count($methods) === 1) {
+            return reset($methods);
+        }
+
+        if ($selectedMethod = $shippingAddress->getShippingMethod()) {
+            foreach ($methods as $method) {
+                if ($method->getCarrierCode() . '_' . $method->getMethodCode() == $selectedMethod) {
+                    return $method;
+                }
+            }
+        }
+        usort($methods, function ($method1, $method2) {
+            return $method1->getPriceInclTax() <=> $method2->getPriceInclTax();
+        });
+        return reset($methods);
+    }
+
+    private function getShippingMethods(AddressInterface $shippingAddress)
+    {
+        $addressId = $shippingAddress->getId();
+        if (isset($this->addressMethods[$addressId])) {
+            return $this->addressMethods[$addressId];
+        }
+        if ($customerAddrId = $shippingAddress->getCustomerAddressId()) {
+            $methods = $this->methodManagement->estimateByAddressId(
+                $shippingAddress->getQuoteId(),
+                $customerAddrId
+            );
+        } else {
+            $methods = $this->methodManagement->estimateByExtendedAddress(
+                $shippingAddress->getQuoteId(),
+                $shippingAddress
+            );
+        }
+        $this->addressMethods[$addressId] = $methods;
+
+        return $this->addressMethods[$addressId];
     }
 }
